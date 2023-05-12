b0bc712eefe9c26850fa69e0ffbc6c9af56ea94d
Rushikesh Mhetre <79018314+rushi11117@users.noreply.github.com>
2023-05-12 05:26:46 +0000

    Update 10 files

--- register-backend/index.js
+++ register-backend/index.js
@@ -1,154 +1,151 @@
 //Packages
-import express from "express"
-import cors from "cors"
-import mongoose from "mongoose"
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 // import { ObjectId } from 'mongodb';
 
 //Models
-import { FreeTime } from './Models/FreeTime.js'
-import { Venue } from './Models/Venues.js'
+import { FreeTime } from "./Models/FreeTime.js";
+import { Venue } from "./Models/Venues.js";
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
 
 //Connection
-mongoose.connect("mongodb://127.0.0.1:27017/playersDB", {
+mongoose.connect(
+  "mongodb+srv://rushimhetre:Rm11117@cluster0.rj98jlm.mongodb.net/?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}, () => {
-    console.log("DB Connected")
-})
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("DB Connected");
+  }
+);
 
 //Schema Defination And Initialization
 app.get("/", (req, res) => {
-    res.send("my API")
-})
+  res.send("my API");
+});
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-})
+  name: String,
+  email: String,
+  password: String,
+});
 
-const User = new mongoose.model("User", userSchema)
+const User = new mongoose.model("User", userSchema);
 
 //Logic Per Route
 
 app.post("/login", (req, res) => {
-    const { email, password } = req.body
-    User.findOne({ email: email }, (err, user) => {
-        console.log(user.password)
-        if (user) {
-            if (password === user.password) {
-                res.send({ message: "Loged in successfully!!", user: user })
-                console.log(user)
-            } else {
-                res.send({ message: "password did't match" })
-            }
-        } else {
-            res.send({ message: "User not exist!!" })
-        }
-    })
-})
+  const { email, password } = req.body;
+  User.findOne({ email: email }, (err, user) => {
+    console.log(user.password);
+    if (user) {
+      if (password === user.password) {
+        res.send({ message: "Loged in successfully!!", user: user });
+        console.log(user);
+      } else {
+        res.send({ message: "password did't match" });
+      }
+    } else {
+      res.send({ message: "User not exist!!" });
+    }
+  });
+});
 
 app.post("/register", (req, res) => {
-    const { name, email, password } = req.body
-    User.findOne({ email: email }, (err, user) => {
-        if (user) {
-            res.send({ message: "user already registered!!" })
+  const { name, email, password } = req.body;
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      res.send({ message: "user already registered!!" });
+    } else {
+      const user = new User({
+        name,
+        email,
+        password,
+      });
+      user.save((err) => {
+        if (err) {
+          res.send(err);
         } else {
-            const user = new User({
-                name,
-                email,
-                password
-            })
-            user.save(err => {
-                if (err) {
-                    res.send(err)
-                } else {
-                    res.send({ message: "user created succesfully!!" })
-                }
-            })
+          res.send({ message: "user created succesfully!!" });
         }
-    })
-
-})
+      });
+    }
+  });
+});
 
 //venuefr Venue From Frontend
-app.put('/addfreetime', async function (req, res) {
-    console.log("Backedn Excecuted")
-    const from = req.body.from
-    const till = req.body.till
-    const venuefr = req.body.venue
-    const email = "rushimhetre@gmail.com"
+app.put("/addfreetime", async function (req, res) {
+  console.log("Backedn Excecuted");
+  const from = req.body.from;
+  const till = req.body.till;
+  const venuefr = req.body.venue;
+  const email = "rushimhetre@gmail.com";
 
-    //Temporarly hardcoded
-    // const isverified = true
+  //Temporarly hardcoded
+  // const isverified = true
 
+  FreeTime.findOne({ email: email }, async (err, freetime) => {
+    if (freetime) {
+      console.log({ message: "Update" });
 
+      try {
+        const updatedFreetime = await FreeTime.updateOne(
+          { _id: freetime._id },
+          {
+            $push: {
+              slot: {
+                from,
+                till,
+                venuefr,
+              },
+            },
+          }
+        );
+        console.log({ message: "Slot Added Successfully!!!" });
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      const freeTime = new FreeTime({
+        email,
+        slot: [{ from, till, venuefr }],
+      });
 
-    FreeTime.findOne({ email: email }, async (err, freetime) => {
-
-        if (freetime) {
-            console.log({ message: "Update" });
-
-            try {
-                const updatedFreetime = await FreeTime.updateOne(
-                    { _id: freetime._id },
-                    {
-                        $push: {
-                            slot: {
-                                from,
-                                till,
-                                venuefr
-                            }
-                        }
-                    }
-                );
-                console.log({ message: "Slot Added Successfully!!!" });
-            } catch (err) {
-                console.log(err);
-            }
-
+      freeTime.save((err) => {
+        if (err) {
+          console.log(err);
         } else {
-            const freeTime = new FreeTime({
-                email,
-                slot: [{ from, till, venuefr }]
-            });
-
-            freeTime.save(err => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log({ message: "Slot Added Successfully!!!" });
-                }
-            });
+          console.log({ message: "Slot Added Successfully!!!" });
         }
-    });
+      });
+    }
+  });
 
-    Venue.findOne({ email: email }, async (err, venue) => {
-        if (venue) {
-            console.log("eat 1* 2* 3* 4* 5*")
+  Venue.findOne({ email: email }, async (err, venue) => {
+    if (venue) {
+      console.log("eat 1* 2* 3* 4* 5*");
+    } else {
+      const venue = new Venue({
+        email,
+        venuefr,
+      });
+      venue.save((err) => {
+        if (err) {
+          console.log(err);
         } else {
-            const venue = new Venue({
-                email,
-                venuefr
-            })
-            venue.save(err => {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log({ message: "Venue Added succesfully!!" })
-                }
-            })
-
+          console.log({ message: "Venue Added succesfully!!" });
         }
-    })
-
+      });
+    }
+  });
 });
 
 app.listen(9002, () => {
-    console.log("DB started on port 9002")
-})
+  console.log("DB started on port 9002");
+});
--- register/src/App.js
+++ register/src/App.js
@@ -1,64 +1,61 @@
-import './App.css';
-import Homepage from './components/content/homepage/homepage';
+import "./App.css";
+import Homepage from "./components/content/homepage/Homepage";
 import Login from "./components/content/login/Login";
-import Register from "./components/content/register/register";
+import Register from "./components/content/register/Register";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useState } from 'react';
-import Header from './header/Header';
-import Footer from './footer/Footer';
-import GamesNear from './components/content/GamesNear/GamesNear'
-import AboutUs from './components/content/AboutUs/AboutUs';
-import Help from './components/content/help/Help';
-import AddGame from './components/content/AddGame/AddGame'
+import { useState } from "react";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import GamesNear from "./components/content/GamesNear/GamesNear";
+import AboutUs from "./components/content/AboutUs/AboutUs";
+import Help from "./components/content/help/Help";
+import AddGame from "./components/content/AddGame/AddGame";
 // import { UserProvider } from './Context';
-import Logout from './components/content/Logout/Logout';
-
+import Logout from "./components/content/Logout/Logout";
 
 function App() {
-
-  const [user, setLoginUser] = useState({})
+  const [user, setLoginUser] = useState({});
   return (
     <Router>
       {/* <UserProvider> */}
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route exact path="/">
-              {
-                user && user._id ? <Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser} />
-              }
-            </Route>
-            <Route path="/login">
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            {user && user._id ? (
+              <Homepage setLoginUser={setLoginUser} />
+            ) : (
               <Login setLoginUser={setLoginUser} />
-            </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <Route path="/logout">
-              <Logout />
-            </Route>
-            <Route path="/gamesnear">
-              <GamesNear />
-            </Route>
-            <Route path="/help">
-              <Help />
-            </Route>
-            <Route path="/about">
-              <AboutUs />
-            </Route>
-            <Route path="/newgame">
-              <AddGame />
-            </Route>
-          </Switch>
-          <Footer />
-        </div>
+            )}
+          </Route>
+          <Route path="/login">
+            <Login setLoginUser={setLoginUser} />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/logout">
+            <Logout />
+          </Route>
+          <Route path="/gamesnear">
+            <GamesNear />
+          </Route>
+          <Route path="/help">
+            <Help />
+          </Route>
+          <Route path="/about">
+            <AboutUs />
+          </Route>
+          <Route path="/newgame">
+            <AddGame />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
       {/* </UserProvider> */}
     </Router>
   );
 }
-
-
-
 
 // function App() {
 //   return (
